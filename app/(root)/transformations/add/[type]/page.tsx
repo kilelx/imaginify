9ad707e6a9230381dffb54f59162f998fe989558
kilelx <llx.kieran@gmail.com>
2024-09-04 11:26:21 +0200

    before implement onXHandler functions

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -25,12 +25,14 @@ const AddTransformationTypePage = async ({params: {type}}: SearchParamProps) => 
          subtitle={transformation.subTitle}
       />
 
-      <TransformationForm
-        action="Add"
-        userId={user._id}
-        type={transformation.type as TransformationTypeKey}
-        creditBalance={user.creditBalance}
-      />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   )
 }
