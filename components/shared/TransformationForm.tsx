57c96ab1036d5fcc93e5eeeec3ffa3a269f33e00
kilelx <llx.kieran@gmail.com>
2024-09-05 17:03:09 +0200

    setup media uploader

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -19,6 +19,7 @@ import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { Button } from "../ui/button";
 import { updateCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -202,6 +203,23 @@ function TransformationForm({ action, data = null, userId, type, creditBalance, 
             )}
           </div>
         )}
+
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({field}) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
 
       <div className="flex flex-col gap-4">
         <Button
