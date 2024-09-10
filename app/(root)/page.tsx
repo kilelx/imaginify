8e761eb87b09e2ba314a4bf1eebdd6df550e3e4f
kilelx <llx.kieran@gmail.com>
2024-09-10 11:17:32 +0200

    implementing: homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,33 @@
-import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { navLinks } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
 
-function Home() {
+const Home = () => {
   return (
-    <div>
-      <p>Home</p>
-      {/* <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
-    </div>
-  );
+    <>
+      <section className="home">
+        <h1 className="home-heading">Unleash your creative vision with Imaginify</h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map(link => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center w-fit rounded-full bg-white p-4">
+                <Image
+                  src={link.icon}
+                  alt="image"
+                  width={24}
+                  height={24}
+                />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+    </>
+  )
 }
-export default Home;
+export default Home
